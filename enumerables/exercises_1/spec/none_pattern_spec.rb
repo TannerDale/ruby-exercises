1ef08ceb6bba55f810c3f767db96b103ec8afdf6
Tanner Dale <tanner@tannerdale.com>
2021-04-01 20:45:08 -0500

    Completed Enumerables

--- enumerables/exercises_1/spec/none_pattern_spec.rb
+++ enumerables/exercises_1/spec/none_pattern_spec.rb
@@ -9,37 +9,67 @@ RSpec.describe 'none pattern' do
     expect(none_broken).to eq(true)
   end
 
-  xit 'double negative' do
+  it 'double negative' do
     numbers = [9, 3, 3, 7, 6, -5, 1]
     not_none_negative = true
     numbers.each do |number|
-      # Your code goes here
+      if number < 0
+        not_none_negative = false
+        break
+      end
     end
     expect(not_none_negative).to eq(false)
   end
 
-  xit 'none are negative' do
+  it 'none are negative' do
     numbers = [9, 3, 1, 8, 3, 3, 5]
     none_negative = true
-    # Your code goes here
+    numbers.each do |number|
+      if number < 0
+        none_negative = false
+        break
+      end
+    end
+
     expect(none_negative).to eq(true)
   end
 
-  xit 'none shall pass' do
+  it 'none shall pass' do
     critters = ["elf", "hobbit", "dwarf", "wizard", "human"]
-    # Your code goes here
+    none_shall_pass = true
+    critters.each do |critter|
+      if critter.include?("pass")
+        none_shall_pass = true
+        break
+      end
+    end
+
     expect(none_shall_pass).to eq(true)
   end
 
-  xit 'one or more shall pass' do
+  it 'one or more shall pass' do
     phrases = ["go", "left", "can cross", "shall pass", "must stay", "tarried"]
-    # Your code goes here
+    none_shall_pass = true
+    phrases.each do |phrase|
+      if phrase.include?("pass")
+        none_shall_pass = false
+        break
+      end
+    end
+
     expect(none_shall_pass).to eq(false)
   end
 
-  xit 'none even' do
+  it 'none even' do
     numbers = [3, 9, 15, 21, 19]
-    # Your code goes here
+    none_even = true
+    numbers.each do |number|
+      if number.even?
+        none_even = false
+        break
+      end
+    end
+
     expect(none_even).to eq(true)
   end
 end
